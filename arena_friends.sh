140e3a4783e7416f24159c53f11240f1d815e6e1
Luc Larocque <56199140+luclarocque@users.noreply.github.com>
2020-03-04 10:27:50 -0800

    Update arena_friends.sh

--- arena_friends.sh
+++ arena_friends.sh
@@ -1,1 +1,1 @@
-curl -s https://play.battlesnake.com/leaderboard/ | grep -E -i -B 1 '(cstafie|kabby|nagol|lucwashere)'
+curl -s https://play.battlesnake.com/arena/global/ | grep -E -i -B 1 '(cstafie|kabby|nagol|lucwashere)'
